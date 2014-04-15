79825c17ebe509e2eb3a7058f4092e8c75e7655b
walkongrass <lizhu.zhanglz@gmail.com>
2014-04-15 21:08:19 +0800

    add a new column into project_group_task for recording the project_group_task status

--- src/sql/create.project_group_task.sql
+++ src/sql/create.project_group_task.sql
@@ -1,9 +1,10 @@
-CREATE  TABLE project_group_task (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `pro_dep_id` INT NOT NULL ,
-  `exe_id` INT NULL ,
-  `status` CHAR(1) NOT NULL DEFAULT 0 COMMENT '0: init; 1: success;2:failed' ,
-  `gmt_created` TIMESTAMP NOT NULL ,
-  `gmt_modified` TIMESTAMP NOT NULL ,
-  PRIMARY KEY (`id`) );
-
+CEATE TABLE `project_group_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pro_dep_id` int(11) NOT NULL,
+  `exe_id` int(11) DEFAULT NULL,
+  `status` char(1) NOT NULL DEFAULT '0' COMMENT '0: init; 1: success;2:failed',
+  `gmt_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `gmt_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `task_status` char(1) NOT NULL DEFAULT '0' COMMENT '0: init;1: executed;',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
